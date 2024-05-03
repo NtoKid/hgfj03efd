f93641b9288c91edcba09595d11b01959d0fbdce
NtoKid <147078600+NtoKid@users.noreply.github.com>
2024-05-03 19:44:17 +0200

    setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,29 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 & {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+
+
+New-Item -ItemType "directory" -Force -Path "c:\down"
+New-Item -ItemType "directory" -Force -Path "c:\rclone"
+New-Item -ItemType "directory" -Force -Path "c:\TOOLS2"
+New-Item -ItemType "directory" -Force -Path "C:\Users\runneradmin\AppData\Roaming\GHISLER\"
+New-Item -ItemType "directory" -Force -Path "c:\rclone\UPLOAD"
+
+<# DOWNLOAD WINFSP RAR #>
+Invoke-WebRequest -Uri "https://www.dropbox.com/s/um5s6m08uzplapm/winfsp.msi?dl=1" -OutFile C:\TOOLS2\winfsp.msi
+
+<# DOWNLOAD TOTAL COMMANDER #>
+Invoke-WebRequest -Uri "https://www.dropbox.com/s/ebdtcbgrnwo97ow/TOTAL.zip?dl=1" -OutFile C:\down\TCM.zip
+Expand-Archive -LiteralPath 'C:\down\TCM.zip' -DestinationPath C:\
+
+<# DOWNLOAD TOTAL COMMANDER CONFIGS
+Invoke-WebRequest -Uri "https://www.dropbox.com/s/tiucda3gzf1xns0/WINCMD.INI?dl=1" -OutFile C:\down\TCM-CFG.zip
+Expand-Archive -LiteralPath 'C:\down\TCM-CFG.zip' -DestinationPath C:\Users\runneradmin\AppData\Roaming\GHISLER #>
+Invoke-WebRequest -Uri "https://www.dropbox.com/s/tiucda3gzf1xns0/WINCMD.INI?dl=1" -OutFile C:\Users\runneradmin\AppData\Roaming\GHISLER\WINCMD.INI
+
+<# DOWNLOAD TOTAL COMMANDER SHORTCUT #>
+Invoke-WebRequest -Uri "https://www.dropbox.com/s/v8stvajyo3jmjz7/Total.bat?dl=1" -OutFile C:\Users\runneradmin\Desktop\Total.bat
+
+<# DOWNLOAD RAR #>
+Invoke-WebRequest -Uri "https://www.dropbox.com/s/28oxq06myogvu87/RAR.zip?dl=1" -OutFile C:\down\RAR.zip
+Expand-Archive -LiteralPath 'C:\down\RAR.zip' -DestinationPath c:\K\WORK\
