af04ec9346961ae9e7bdc00ae616258fa4e3a275
NtoKid <147078600+NtoKid@users.noreply.github.com>
2024-05-03 18:53:46 +0200

    setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,12 +1,16 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
+& {$P = $env:TEMP + '\chrome_installer.exe'; Invoke-WebRequest 'https://dl.google.com/chrome/install/latest/chrome_installer.exe' -OutFile $P; Start-Process -FilePath $P -Args '/install' -Verb RunAs -Wait; Remove-Item $P}
+
 New-Item -ItemType "directory" -Force -Path "c:\down"
 New-Item -ItemType "directory" -Force -Path "c:\rclone"
 New-Item -ItemType "directory" -Force -Path "c:\TOOLS2"
 New-Item -ItemType "directory" -Force -Path "C:\Users\runneradmin\AppData\Roaming\GHISLER\"
 New-Item -ItemType "directory" -Force -Path "c:\rclone\UPLOAD"
+
+
 <# DOWNLOAD WINFSP RAR #>
-Invoke-WebRequest -Uri "https://www.dropbox.com/s/um5s6m08uzplapm/winfsp.msi?dl=1" -OutFile C:\TOOLS2\winfsp.msi
+Invoke-WebRequest -Uri "cc" -OutFile C:\TOOLS2\winfsp.msi
 
 <# DOWNLOAD TOTAL COMMANDER #>
 Invoke-WebRequest -Uri "https://www.dropbox.com/s/ebdtcbgrnwo97ow/TOTAL.zip?dl=1" -OutFile C:\down\TCM.zip
@@ -24,8 +28,12 @@ Invoke-WebRequest -Uri "https://www.dropbox.com/s/v8stvajyo3jmjz7/Total.bat?dl=1
 Invoke-WebRequest -Uri "https://www.dropbox.com/s/28oxq06myogvu87/RAR.zip?dl=1" -OutFile C:\down\RAR.zip
 Expand-Archive -LiteralPath 'C:\down\RAR.zip' -DestinationPath c:\K\WORK\
 
+
+
 <# INSTALL WINSFP#>
 msiexec /i "C:\TOOLS2\winfsp.msi" /q
+
+
 <# WALLPAPER #>
 Invoke-WebRequest -Uri "https://img89.pixhost.to/images/626/381484029_zippy001.jpg" -OutFile C:\wallpaper.jpg
 $MyWallpaper="C:\wallpaper.jpg"
@@ -207,9 +215,6 @@ Add-Type $pinvokeCode -ErrorAction SilentlyContinue
 
 Set-ScreenResolution -Width 1920 -Height 1080
 
-<# SHELL INSTALLS #>
-pip install tqdm
-pip install requests
 
 <# START TOTAL COMMANDER #>
 Start-Process -FilePath "C:\TOTAL\TOTALCMD.exe" -WindowStyle Minimized
